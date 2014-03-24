ee692613a4c17c640f0fdbc2f3043ca0a29a7e3a
Ronan Collobert <ronan@collobert.com>
2014-03-24 16:58:59 +0100

    doc: fonts

--- fontface.lua
+++ fontface.lua
@@ -3,6 +3,13 @@ local class = require 'class'
 local ffi = require 'ffi'
 local cairo = require 'cairo.env'
 local C = cairo.C
+local doc = require 'argcheck.doc'
+
+doc[[
+
+### Cairo Fonts
+
+]]
 
 local FontFace = class.new('cairo.FontFace')
 cairo.FontFace = FontFace
@@ -23,6 +30,19 @@ FontFace.__init = argcheck{
 }
 
 FontFace.status = argcheck{
+   doc = [[
+<a name="FontFace.status">
+#### FontFace.status(@ARGP)
+
+@ARGT
+
+Checks whether an error has previously occurred for this
+font face
+
+_Return value_: [`"success"`](enums.md#Status) or another error such as
+[`"no-memory"`](enums.md#Status).
+
+]],
    {name="self", type="cairo.FontFace"},
    call =
       function(self)
@@ -31,6 +51,18 @@ FontFace.status = argcheck{
 }
 
 FontFace.getType = argcheck{
+   doc = [[
+<a name="FontFace.getType">
+#### FontFace.getType(@ARGP)
+
+@ARGT
+
+This function returns the type of the backend used to create
+a font face. See [`FontType`](enums.md#FontType) for available types.
+
+_Return value_: The type of `font_face`.
+
+]],
    {name="self", type="cairo.FontFace"},
    call =
       function(self)
@@ -42,10 +74,24 @@ local ScaledFontFace = class.new('cairo.ScaledFontFace', 'cairo.FontFace')
 cairo.ScaledFontFace = ScaledFontFace
 
 ScaledFontFace.__init = argcheck{
+   doc = [[
+<a name="ScaledFontFace.new">
+#### ScaledFontFace.new(@ARGP)
+
+@ARGT
+
+Creates a [`ScaledFontFace`](#ScaledFontFace) object from a font face and matrices that
+describe the size of the font and the environment in which it will
+be used.
+
+_Return value_: a newly created [`ScaledFontFace`](#ScaledFontFace). Destroy with
+[`ScaledFontFace.destroy()`](#ScaledFontFace.destroy)
+
+]],
    {name="self", type="cairo.ScaledFontFace"},
-   {name="font_matrix", type="cairo.Matrix"},
-   {name="ctm", type="cairo.Matrix"},
-   {name="options", type="cairo.FontOptions"},
+   {name="font_matrix", type="cairo.Matrix", doc="font space to user space transformation matrix for the font. In the simplest case of a N point font, this matrix is just a scale by N, but it can also be used to shear the font or stretch it unequally along the two axes. See [`Context.setFontMatrix()`](context.md#Context.setFontMatrix)."},
+   {name="ctm", type="cairo.Matrix", doc="user to device transformation matrix with which the font will be used."},
+   {name="options", type="cairo.FontOptions", doc="options to use when getting metrics for the font and rendering with it."},
    call =
       function(self, font_matrix, ctm, options)
          self.C = C.cairo_scaled_font_create(self.C, font_matrix.C, ctm.C, options.C)
@@ -58,10 +104,31 @@ local ToyFontFace = class.new('cairo.ToyFontFace', 'cairo.FontFace')
 cairo.ToyFontFace = ToyFontFace
 
 ToyFontFace.__init = argcheck{
+   doc = [[
+<a name="ToyFontFace.new">
+#### ToyFontFace.new(@ARGP)
+
+@ARGT
+
+Creates a font face from a triplet of family, slant, and weight.
+These font faces are used in implementation of the the [`Context`](context.md#Context) "toy"
+font API.
+
+If `family` is the zero-length string "", the platform-specific default
+family is assumed.  The default family then can be queried using
+[`ToyFontFace.getFamily()`](#ToyFontFace.getFamily).
+
+The [`Context.selectFontFace()`](context.md#Context.selectFontFace) function uses this to create font faces.
+See that function for limitations and other details of toy font faces.
+
+_Return value_: a newly created [`FontFace`](#FontFace). Free with
+[`FontFace.destroy()`](#FontFace.destroy) when you are done using it.
+
+]],
    {name="self", type="cairo.ToyFontFace"},
-   {name="family", type="string"},
-   {name="slant", type="string"},
-   {name="weight", type="string"},
+   {name="family", type="string", doc="a font family name, encoded in UTF-8"},
+   {name="slant", type="string", doc="the slant for the font"},
+   {name="weight", type="string", doc="the weight for the font"},
    call =
       function(self, family, slant, weight)
          self.C = C.cairo_toy_font_face_create(family, cairo.enums.FontSlant[slant], cairo.enums.FontWeight[weight])
@@ -71,6 +138,18 @@ ToyFontFace.__init = argcheck{
 }
 
 ToyFontFace.getFamily = argcheck{
+   doc = [[
+<a name="ToyFontFace.getFamily">
+#### ToyFontFace.getFamily(@ARGP)
+
+@ARGT
+
+Gets the familly name of a toy font.
+
+_Return value_: The family name.  This string is owned by the font face
+and remains valid as long as the font face is alive (referenced).
+
+]],
    {name="self", type="cairo.ToyFontFace"},
    call =
       function(self)
@@ -79,6 +158,17 @@ ToyFontFace.getFamily = argcheck{
 }
 
 ToyFontFace.getSlant = argcheck{
+   doc = [[
+<a name="ToyFontFace.getSlant">
+#### ToyFontFace.getSlant(@ARGP)
+
+@ARGT
+
+Gets the slant a toy font.
+
+_Return value_: The slant value
+
+]],
    {name="self", type="cairo.FontFace"},
    call =
       function(self)
@@ -87,6 +177,17 @@ ToyFontFace.getSlant = argcheck{
 }
 
 ToyFontFace.getWeight = argcheck{
+   doc = [[
+<a name="ToyFontFace.getWeight">
+#### ToyFontFace.getWeight(@ARGP)
+
+@ARGT
+
+Gets the weight a toy font.
+
+_Return value_: The weight value
+
+]],
    {name="self", type="cairo.FontFace"},
    call =
       function(self)
